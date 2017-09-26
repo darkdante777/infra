50caa4d71a3148a63cd6bc37fbff3871d2d54c27
darkdante777 <contacts777@ya.ru>
2017-09-26 18:24:07 +0300

    homework N 12 by Sergey Radchenko

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -17,7 +17,7 @@ variable private_key_path {
 
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default     = "reddit-app-base"
+  default     = " reddit-app-base"
 }
 
 variable db_disk_image {
